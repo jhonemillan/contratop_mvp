8320fb9cfe447a33040f6f134f0ba0d70ff22aa4
root <root@pop-os.localdomain>
2019-08-10 08:39:28 -0500

    mvp contact

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'contratopmvp';
+  countriesSelect: Array<any>;
+
+  ngOnInit() {
+    this.countriesSelect = [
+    { value: '1', label: 'Option 1' },
+    { value: '2', label: 'Option 2' },
+    { value: '3', label: 'Option 3' },
+    ];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { MDBBootstrapModulesPro } from 'ng-uikit-pro-standard';
+// MDB Angular Pro
+import { WavesModule, SelectModule } from 'ng-uikit-pro-standard'
+import { MDBSpinningPreloader } from 'ng-uikit-pro-standard';
 import { NgModule } from '@angular/core';
-
+// Angular Animations Module
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,9 +13,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SelectModule,
+    WavesModule,
+    BrowserAnimationsModule,
+    MDBBootstrapModulesPro.forRoot()
   ],
-  providers: [],
+  providers: [MDBSpinningPreloader],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
